3205f63f86f22862ef0798fb5f9c2be465e2ffd2
ExcelsKennedy2 <kennedymbugua841@gmail.com>
2025-02-17 12:59:48 +0300

    Second commit

---
+++ core/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('services/', views.services, name='services'),
+    path('details/', views.details, name='details'),
+]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def details(request):
+    return render(request, 'portfolio-details.html')
+
+def services(request):
+    return render(request, 'service-details.html')
+
+def base(request):
+    return render(request, 'base.html')

b22d6dedc596797c297c6f6145ac95d589b863ca
ExcelsKennedy2 <kennedymbugua841@gmail.com>
2025-07-28 12:52:37 +0300

    added a bot to keep the app active

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('', include('pwa.urls')),
+    path("ping/", lambda request: HttpResponse("pong")),
 ]
